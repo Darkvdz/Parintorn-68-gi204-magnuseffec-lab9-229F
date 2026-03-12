00e971f91f2b38553160d8efd84a5b9ac605416e
Darkvdz <231534505+Darkvdz@users.noreply.github.com>
2026-03-12 09:59:12 +0700

    AngularVelocity-Script

--- Assets/Script/AngularVelocity.cs
+++ Assets/Script/AngularVelocity.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class AngularVelocity : MonoBehaviour
 {
+    Rigidbody rb;
+    [SerializeField] private float angularSpeed;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Keyboard.current.aKey.isPressed)
+        {
+            //rb.angularVelocity = new Vector3(0, angularSpeed, 0);
+            rb.angularVelocity = Vector3.up * angularSpeed;
+        }
+        else
+        {
+            rb.angularVelocity = Vector3.zero;
+        }
     }
 }
